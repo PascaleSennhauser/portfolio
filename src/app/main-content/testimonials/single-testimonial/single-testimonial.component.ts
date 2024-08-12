d05be167a806b84eb599999ee31522775402e4f6
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-08-12 18:37:54 +0200

    Added project structure

---
+++ src/app/main-content/testimonials/single-testimonial/single-testimonial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-testimonial',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-testimonial.component.html',
+  styleUrl: './single-testimonial.component.scss'
+})
+export class SingleTestimonialComponent {
+
+}
