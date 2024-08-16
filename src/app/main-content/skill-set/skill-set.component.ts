59c84d61fd2bf84913949d8bc974d4d14b57b9d4
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-08-16 19:15:38 +0200

    Added skill set

--- src/app/main-content/skill-set/skill-set.component.ts
+++ src/app/main-content/skill-set/skill-set.component.ts
@@ -8,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  skills: Array<any> = [
+    {
+      name: 'Angular',
+      img: 'Angular.png'
+    },
+    {
+      name: 'TypeScript',
+      img: 'TypeScript.png'
+    },
+    {
+      name: 'JavaScript',
+      img: 'JavaScript.png'
+    },
+    {
+      name: 'HTML',
+      img: 'html.png'
+    },
+    {
+      name: 'Scrum',
+      img: 'Scrum.png'
+    },
+    {
+      name: 'Firebase',
+      img: 'Firebase.png'
+    },
+    {
+      name: 'Git',
+      img: 'Git.png'
+    },
+    {
+      name: 'CSS',
+      img: 'css.png'
+    },
+    {
+      name: 'REST-Api',
+      img: 'Api.png'
+    },
+    {
+      name: 'Material Design',
+      img: 'Material-des.png'
+    }
+  ];
 
 }
