8a39a3895882a8380fdd735d430ce83ad2c24e79
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-08-22 17:05:13 +0200

    Added class language-bold, when specific language is selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
+import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,5 @@ import { FooterComponent } from './shared/footer/footer.component';
 })
 export class AppComponent {
   title = 'portfolio';
+  languageData = inject(LanguageService);
 }
