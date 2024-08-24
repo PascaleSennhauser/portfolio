532781ad7add227d11468fe12e55645e1503d30b
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-08-25 00:56:30 +0200

    Worked on the contact-form

--- src/app/main-content/contact/contact-form/contact-form.component.ts
+++ src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -1,17 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { LanguageService } from '../../../services/language.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 
 export class ContactFormComponent {
   languageData = inject(LanguageService);
+  noValidName: boolean = false;
+  name = "";
+  isFirstNameInput: boolean = true;
+  noValidMail: boolean = false;
+  email = "";
+  isFirstMailInput: boolean = true;
+  noValidMessage: boolean = false;
+  message = "";
+  isFirstMessageInput: boolean = true;
 
   /**
    * Constructor for the component, which initilizes the Router.
@@ -28,4 +39,42 @@ export class ContactFormComponent {
   goToImprint() {
     this.router.navigateByUrl(this.languageData.currentLanguage + '/imprint');
   }
+
+
+  checkNameInput() {
+    if (this.name.length < 2) {
+      this.noValidName = true;
+    } else {
+      this.noValidName = false;
+    }
+  }
+
+  firstNameInput() {
+    this.isFirstNameInput = false;
+  }
+
+  checkMailInput() {
+    if (this.email.length < 2) {
+      this.noValidMail = true;
+    } else {
+      this.noValidMail = false;
+    }
+  }
+
+  firstMailInput() {
+    this.isFirstMailInput = false;
+  }
+
+  checkMessageInput() {
+    if (this.message.length < 2) {
+      this.noValidMessage = true;
+    } else {
+      this.noValidMessage = false;
+    }
+  }
+
+  firstMessageInput() {
+    this.isFirstMessageInput = false;
+  }
+
 }
